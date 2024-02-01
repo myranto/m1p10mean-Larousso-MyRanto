3ab55609682be7ee66a04290be61efd18fd892cd
Lars Ratovo <lars.ratovo@gmail.com>
2024-02-01 23:17:01 +0300

    show dialog

--- frontend/src/app/admin/service/form/form.component.ts
+++ frontend/src/app/admin/service/form/form.component.ts
@@ -1,31 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule} from '@angular/material/input';
 import { Service } from '../../../interfaces/service';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 import {
-  MatDialog,
   MAT_DIALOG_DATA,
   MatDialogRef,
   MatDialogTitle,
   MatDialogContent,
-  MatDialogActions,
   MatDialogClose,
+  MatDialogModule
 } from '@angular/material/dialog';
 @Component({
   selector: 'service-form',
   standalone: true,
   imports: [
     MatFormFieldModule,
-    MatGridListModule,
     MatInputModule,
+    FormsModule,
     MatButtonModule,
-
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogClose,
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
-  @Input() model! : Service;
+  constructor(public dialogService : MatDialogRef<FormComponent>,@Inject(MAT_DIALOG_DATA) public model : Service){
+  }
+
+  onCancelClick() : void {
+    this.dialogService.close();
+  }
 }
--- frontend/src/app/admin/service/service.component.ts
+++ frontend/src/app/admin/service/service.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog'
 import { Service } from '../../interfaces/service';
 import { ServiceService } from '../../services/admin/service.service';
 import { FormComponent } from './form/form.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-service',
@@ -11,18 +13,26 @@ import { FormComponent } from './form/form.component';
   imports: [
     MatTableModule,
     FormComponent,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
 export class ServiceComponent {
   models : Service[] = [];
-  headers : string[] = ['name','duration','committee','price'];
+  headers : string[] = ['name','duration','committee','price','actions'];
   state : string = 'idle';
   error : string = '';
   private service : ServiceService = inject(ServiceService);
 
-  constructor (){  
+  constructor ( public dialog : MatDialog){  
     this.service.get().then(serviceList => this.models = serviceList).catch((e) => {this.state = 'error'; this.error = e});
+  }
+  openFormForAdd() : void{
+    const dialogRef = this.dialog.open(FormComponent,{data:{name:'',price:0,committee:0,duration:0}});
+  }
+  openEditForm(model : Service) : void {
+    const dialogRef = this.dialog.open(FormComponent,{data: model});
   }
 }
