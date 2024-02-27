6e427b498443be140d25b4979d9cfffe42242770
myranto <my.randrianantoandro@gmail.com>
2024-02-27 22:21:50 +0300

    reset

--- backend/models/type_cost.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const typeCostSchema = new mongoose.Schema({
-    name:{type: String, required: true, unique:true},
-});
-
-module.exports = mongoose.model('typeCost',typeCostSchema);
